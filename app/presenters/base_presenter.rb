d5fb4fb1d1507509064bbf13453863a76b8cf5b2
Andrei Crudu <andrei.crudu@yahoo.com>
2016-07-24 04:31:53 +0300

    Add a basic base presenter

---
+++ app/presenters/base_presenter.rb
@@ -0,0 +1,15 @@
+class BasePresenter
+  attr_accessor :object, :params, :data
+
+  def initialize(object, params, options = {})
+    @object = object
+    @params = params
+    @options = options
+    @data = HashWithIndifferentAccess.new
+  end
+
+  def as_json(*)
+    @data
+  end
+
+end
