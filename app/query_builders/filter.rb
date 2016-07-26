03fff3d1d25712901829e467a69b36bd13c953a3
Andrei Crudu <andrei.crudu@yahoo.com>
2016-07-27 01:14:48 +0300

    Finish the filter query builder

--- app/query_builders/filter.rb
+++ app/query_builders/filter.rb
@@ -7,7 +7,49 @@ class Filter
 
   def filter
     return @scope unless @filters.any?
+    @filters = format_filters
+    build_filter_scope
     @scope
+  end
+
+  private
+
+  def format_filters
+    @filters.each_with_object({}) do |(key, value), hash|
+      hash[key] = {
+          value: value,
+          column: key.split('_')[0...-1].join('_'),
+          predicate: key.split('_').last
+      }
+    end
+  end
+
+  def build_filter_scope
+    @filters.each do |key, data|
+      @scope = send(data[:predicate], data[:column], data[:value])
+    end
+  end
+
+  def eq(column, value)
+    @scope.where(column => value)
+  end
+  def cont(column, value)
+    @scope.where("#{column} LIKE ?", "%#{value}%")
+  end
+  def notcont(column, value)
+    @scope.where("#{column} NOT LIKE ?", "%#{value}%")
+  end
+  def start(column, value)
+    @scope.where("#{column} LIKE ?", "#{value}%")
+  end
+  def end(column, value)
+    @scope.where("#{column} LIKE ?", "%#{value}")
+  end
+  def gt(column, value)
+    @scope.where("#{column} > ?", value)
+  end
+  def lt(column, value)
+    @scope.where("#{column} < ?", value)
   end
 
 end
--- spec/query_builders/filter_spec.rb
+++ spec/query_builders/filter_spec.rb
@@ -22,8 +22,71 @@ RSpec.describe Filter do
   describe '#filter' do
     context 'without any parameters' do
 
-      it "returns the scope unchanged" do
+      it 'returns the scope unchanged' do
         expect(filtered).to eq scope
+      end
+    end
+
+    context 'with valid parameters' do
+      context 'with "title_eq=Ruby Under a Microscope"' do
+        let(:params) { { 'q' => { 'title_eq' => 'Ruby Under a Microscope' } } }
+
+        it 'gets only "Ruby Under a Microscope" back' do
+          expect(filtered.first.id).to eq ruby_microscope.id
+          expect(filtered.size).to eq 1
+        end
+      end
+
+      context 'with "title_cont=Under"' do
+        let(:params) { { 'q' => { 'title_cont' => 'Under' } } }
+
+        it 'gets only "Ruby Under a Microscope" back' do
+          expect(filtered.first.id).to eq ruby_microscope.id
+          expect(filtered.size).to eq 1
+        end
+      end
+
+      context 'with "title_notcont=Ruby"' do
+        let(:params) { { 'q' => { 'title_notcont' => 'Ruby' } } }
+
+        it 'gets only "Agile Web Development with Rails 4" back' do
+          expect(filtered.first.id).to eq agile_web_dev.id
+          expect(filtered.size).to eq 1
+        end
+      end
+
+      context 'with "title_start=Ruby"' do
+        let(:params) { { 'q' => { 'title_start' => 'Ruby' } } }
+
+        it 'gets only "Ruby Microscope and Ruby on Rails Tutorial" back' do
+          expect(filtered.size).to eq 2
+        end
+      end
+
+      context 'with "title_end=Tutorial"' do
+        let(:params) { { 'q' => { 'title_end' => 'Tutorial' } } }
+
+        it 'gets only "Ruby Microscope and Ruby on Rails Tutorial" back' do
+          expect(filtered.first).to eq rails_tutorial
+        end
+      end
+
+      context 'with "released_on_lt=2013-05-10"' do
+        let(:params) { { 'q' => { 'released_on_lt' => '2013-05-10' } } }
+
+        it 'gets only the book with released_on before 2013-05-10' do
+          expect(filtered.first.title).to eq rails_tutorial.title
+          expect(filtered.size).to eq 1
+        end
+      end
+
+      context 'with "released_on_gt=2013-01-01"' do
+        let(:params) { { 'q' => { 'released_on_gt' => '2014-01-01' } } }
+
+        it 'gets only the book with id 1' do
+          expect(filtered.first.title).to eq agile_web_dev.title
+          expect(filtered.size).to eq 1
+        end
       end
     end
   end
