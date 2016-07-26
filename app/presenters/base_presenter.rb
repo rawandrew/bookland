ecc9e2f2e11cb243c90c8d784f36c1b13e345f14
Andrei Crudu <andrei.crudu@yahoo.com>
2016-07-26 21:46:12 +0300

    Improve further the base presenter

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,30 +1,20 @@
 class BasePresenter
 
-  @relations = []
-  @sort_attributes = []
-  @filter_attributes = []
-  @build_attributes = []
+  CLASS_ATTRIBUTES = {
+      build_with: :build_attributes,
+      related_to: :relations,
+      sort_by: :sort_attributes,
+      filter_by: :filter_attributes
+  }
+
+  CLASS_ATTRIBUTES.each { |k, v| instance_variable_set("@#{v}", []) }
 
   class << self
-    attr_accessor :relations,
-                  :sort_attributes,
-                  :filter_attributes,
-                  :build_attributes
-
-    def related_to(*args)
-      @relations = args.map(&:to_s)
-    end
-
-    def sort_by(*args)
-      @sort_attributes = args.map(&:to_s)
-    end
-
-    def filter_by(*args)
-      @filter_attributes = args.map(&:to_s)
-    end
-
-    def build_with(*args)
-      @build_attributes = args.map(&:to_s)
+    attr_accessor *CLASS_ATTRIBUTES.values
+    CLASS_ATTRIBUTES.each do |k, v|
+      define_method k do |*args|
+        instance_variable_set("@#{v}", args.map(&:to_s))
+      end
     end
   end
 
