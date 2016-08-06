cea92ef80d34e89f74277fa1f31224b44a9ec1bf
Andrei Crudu <andrei.crudu@yahoo.com>
2016-08-06 03:17:32 +0300

    Filter the cover attribute from the logs

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Bookland
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.filter_parameters += [:cover]
   end
 end
