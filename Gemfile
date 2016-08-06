4bcf91b348cfee1c7530c4e72737c8a1d106b6be
Andrei Crudu <andrei.crudu@yahoo.com>
2016-08-06 04:30:47 +0300

    Add the pg_search gem and run its installation

--- Gemfile
+++ Gemfile
@@ -15,6 +15,9 @@ gem 'carrierwave-base64'
 # Kaminari dem for pagination
 gem 'kaminari'
 
+# Use PGSearch for full text search
+gem 'pg_search'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722212100) do
+ActiveRecord::Schema.define(version: 20160806012835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20160722212100) do
     t.index ["isbn_13"], name: "index_books_on_isbn_13", using: :btree
     t.index ["publisher_id"], name: "index_books_on_publisher_id", using: :btree
     t.index ["title"], name: "index_books_on_title", using: :btree
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.string   "searchable_type"
+    t.integer  "searchable_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
   end
 
   create_table "publishers", force: :cascade do |t|
